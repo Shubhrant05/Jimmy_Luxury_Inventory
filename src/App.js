a969255308aeac3e26bb4390b4a67f28f6a43f91
Shubhrant05 <shubhrantyadav34366@gmail.com>
2024-03-20 11:59:27 +0530

    page restriction based on jwt token done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Import Navigate for redirecting
 import 'react-toastify/dist/ReactToastify.css';
 import Auth from './Components/Auth/Auth'
 import Signup from './Components/Auth/Signup';
 import Inventory from './Components/Inventory/index';
 import Settings from './Components/Settings/Settings';
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuthentication = () => {
+      const jwtToken = getCookie('jwt');
+      setAuthenticated(jwtToken !== undefined);
+    };
+
+    checkAuthentication();
+  }, []);
+
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Auth/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/dashboard" element={<Inventory/>} />
-          <Route path="/settings" element={<Settings/>} />
+          <Route path="/" element={<Auth />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/dashboard"
+            element={authenticated ? <Inventory /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/settings"
+            element={authenticated ? <Settings /> : <Navigate to="/" />}
+          />
           <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
       </Router>
